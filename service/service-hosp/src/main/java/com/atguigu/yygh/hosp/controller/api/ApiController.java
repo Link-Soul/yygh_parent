966fc74fd2702fb4dc1ee3f9f0c72e67b297d51b
Link_Soul <1749449185@qq.com>
2023-09-15 17:42:20 +0800

    完成day8 base64图片转换

---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
@@ -0,0 +1,70 @@
+package com.atguigu.yygh.hosp.controller.api;
+
+
+import com.atguigu.yygh.common.exception.YyghException;
+import com.atguigu.yygh.common.helper.HttpRequestHelper;
+import com.atguigu.yygh.common.result.Result;
+
+import com.atguigu.yygh.common.result.ResultCodeEnum;
+import com.atguigu.yygh.common.util.MD5;
+import com.atguigu.yygh.hosp.service.HospitalService;
+import com.atguigu.yygh.hosp.service.HospitalSetService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/hosp")
+public class ApiController {
+
+    @Autowired
+    private HospitalService hospitalService;
+
+    @Autowired
+    private HospitalSetService hospitalSetService;
+
+
+    @ApiOperation(value = "上传医院")
+    @PostMapping("saveHospital")
+    public Result saveHospital(HttpServletRequest request) {
+
+        Map<String, Object> paramMap = HttpRequestHelper.switchMap(request.getParameterMap());
+
+        String hospSign = (String) paramMap.get("sign");
+        //必须参数校验
+        String hoscode = (String)paramMap.get("hoscode");
+
+        if(StringUtils.isEmpty(hoscode)) {
+            throw new YyghException(ResultCodeEnum.PARAM_ERROR);
+        }
+        //传输过程中“+”转换为了“ ”，因此我们要转换回来
+        String logoDataString = (String)paramMap.get("logoData");
+        if(!StringUtils.isEmpty(logoDataString)) {
+            String logoData = logoDataString.replaceAll(" ", "+");
+            paramMap.put("logoData", logoData);
+        }
+
+        //签名校验
+        String signKey = hospitalSetService.getSignKey(hoscode);
+
+//        if(!HttpRequestHelper.isSignEquals(paramMap, signKey)) {
+//            throw new YyghException(ResultCodeEnum.SIGN_ERROR);
+//        }
+        String encrypt = MD5.encrypt(signKey);
+
+        if (!encrypt.equals(hospSign)){
+            throw new YyghException(ResultCodeEnum.SIGN_ERROR);
+        }
+
+        hospitalService.save(paramMap);
+        return Result.ok();
+    }
+
+
+}
---
+++ service/service-hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalServiceImpl.java
@@ -0,0 +1,42 @@
+package com.atguigu.yygh.hosp.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.yygh.hosp.repository.HospitalRepository;
+import com.atguigu.yygh.hosp.service.HospitalService;
+import com.atguigu.yygh.model.hosp.Hospital;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class HospitalServiceImpl implements HospitalService {
+    @Autowired
+    private HospitalRepository hospitalRepository;
+
+    @Override
+    public void save(Map<String, Object> paramMap) {
+        // 把map变成hospital
+        // tojson变成字符串，
+        // log.info(JSONObject.toJSONString(paramMap));
+        Hospital hospital = JSONObject.parseObject(JSONObject.toJSONString(paramMap),Hospital.class);
+        //判断是否存在
+        Hospital targetHospital = hospitalRepository.getHospitalByHoscode(hospital.getHoscode());
+        if(null != targetHospital) {    // 存在 修改
+            hospital.setStatus(targetHospital.getStatus());
+            hospital.setCreateTime(targetHospital.getCreateTime());
+            hospital.setUpdateTime(new Date());
+            hospital.setIsDeleted(0);
+            hospitalRepository.save(hospital);
+        } else {                        // 不存在 添加
+            //0：未上线 1：已上线
+            hospital.setStatus(0);
+            hospital.setCreateTime(new Date());
+            hospital.setUpdateTime(new Date());
+            hospital.setIsDeleted(0);
+            hospitalRepository.save(hospital);
+        }
+    }
+
+}
